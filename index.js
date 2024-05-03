267d7bb5f48baed7b17c93f888ccf49a1e848cc9
EricWithC04 <negativegame117@gmail.com>
2024-05-03 15:15:37 -0300

    feat: execute python code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import helmet from "helmet"
 import vm from "vm"
+import { PythonShell } from "python-shell"
 
 const app = express()
 
@@ -17,11 +18,20 @@ app.post("/execute", (req, res) => {
     }
 
     try {
-        const sandbox = { console }
-        const context = vm.createContext(sandbox)
-        const result = vm.runInContext(code, context)
+        // const options = {
+        //     mode: "text",
+        //     pythonPath: "C:/Users/merca/AppData/Local/Programs/Python/Python312/python.exe",
+        //     pythonOptions: ["-u"],
+        //     args: ['']
+        // }
 
-        return res.status(200).json({ result })
+        // const sandbox = { console }
+        // const context = vm.createContext(sandbox)
+        // const result = vm.runInContext(code, context)
+
+        PythonShell.runString(code, null).then(([result]) => console.log(result))
+
+        return res.status(200).json({ "hola": "hola" })
     } catch (err) {
         console.error({ msg: "Error al ejecutar el codigo", error: err.message });
     }
